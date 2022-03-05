8f5ee81db9b5d02789d2a1cd998549efc77b8eab
Esteban BS <estbs93@gmail.com>
2022-03-05 15:33:39 -0500

    Feat: Refactor the test

--- spec/school_spec.rb
+++ spec/school_spec.rb
@@ -8,13 +8,15 @@ class School
 end
 
 RSpec.describe 'school' do
+  before do
+    @school = School.new('Beverly Hills High School')
+  end
+
   it 'has a name' do
-    school = School.new('Beverly Hills High School')
-    expect(school.name).to eq('Beverly Hills High School')
+    expect(@school.name).to eq('Beverly Hills High School')
   end
 
   it 'should start off with no students' do
-    school = School.new('Notre Dame High School')
-    expect(school.students).to eq([])
+    expect(@school.students).to eq([])
   end
 end
